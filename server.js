930cb639628026439d65b6d7cbfb729225706374
Qayyum Suhaimi <qayyumsuhaimi10@gmail.com>
2024-08-21 22:42:49 +0800

    Add Swagger API

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 const fastify = require('fastify')({ logger: true })
+
+// Registering Swagger
+fastify.register(require('@fastify/swagger'), {
+    swagger: {
+        info: { title: 'fastify-api', version: '0.1.0' },
+    },
+})
+
+// Registering Swagger UI
+fastify.register(require('@fastify/swagger-ui'), {
+    routePrefix: '/docs',
+    exposeRoute: true,
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+    transformSpecificationClone: true
+})
+
 fastify.register(require('./routes/items'))
 
 const PORT = 5000
